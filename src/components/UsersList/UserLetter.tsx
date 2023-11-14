1d479a36ef579f5e40c863816880ff30f9836a89
Kamil Lewiński <kml.lewinski@gmail.com>
2023-11-14 14:33:24 +0100

    fixed links and displaying text in letter boxes

--- src/components/UsersList/UserLetter.tsx
+++ src/components/UsersList/UserLetter.tsx
@@ -2,9 +2,23 @@ import React, { useContext, useEffect, useState } from 'react'
 import { getAbsolutePhotoURL } from '../../context/functions'
 import { ManageContentContext } from '../../context/elementsOnBoardContext/ElementsOnBoardContext';
 const UserLetter = () => {
-
+  const [letter, setLetter] = useState<string>('');
   const state = useContext(ManageContentContext);
   const user = state?.user;
+  
+  
+  useEffect(()=>{
+    const userLetter = user?.letter;
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+
+    if(userLetter){
+      setLetter(userLetter?.replace(urlRegex, (url)=>{
+        return `<a class='textLink' href="${url}" target="_blank">Link</a>`;
+      }))
+      console.log(letter);
+    }
+
+  }, [user])
   const [photoAURL, setPhotoAURL] = useState<string>('');
   useEffect(()=>{
     const fetchPhotoURL = async () => {
@@ -24,7 +38,7 @@ const UserLetter = () => {
         </div>
         {user?.letter !== '' ? 
         <div className='letterContent'>
-            <pre>{user?.letter}</pre>
+          <pre dangerouslySetInnerHTML={{ __html: letter }} />
         </div>
        :
        <div className='letterContent'>Ten użytkonik nie ma jeszcze listu</div> 
